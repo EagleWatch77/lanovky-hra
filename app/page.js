e2405442a2dce0a8a4348efcd972c0d994c07e5f
EagleWatch77 <mirko@droppa.net>
2026-07-15 15:45:45 +0200

    Change map image style to cover

--- app/page.js
+++ app/page.js
@@ -197,7 +197,7 @@ export default function PrehladPage() {
       <img
         src={mapaObrazok}
         alt="Mapa strediska"
-        style={{ position: "absolute", inset: 0, width: "100%", height: "100%", objectFit: "contain", objectPosition: "center" }}
+        style={{ position: "absolute", inset: 0, width: "100%", height: "100%", objectFit: "cover", objectPosition: "center center" }}
       />
 
       {/* Plávajúci zhluk vľavo hore — logo, názov, štatistiky */}
