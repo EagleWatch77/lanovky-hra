5acd648d6c39ccc3ca57918676478ee19dfc16d7
EagleWatch77 <mirko@droppa.net>
2026-07-26 21:39:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -258,7 +258,7 @@ export default function PrehladPage() {
 
       {okno === "ceny" && (
         <WindowModal title="💶 Ceny" onClose={() => setOkno(null)} width={480}>
-          <CenyOkno stanica={stanica} budovy={budovy} zmenitCenu={zmenitCenu} />
+          <CenyOkno stanica={stanica} budovy={budovy} zmenitCenu={zmenitCenu} zmenitPrevadzkovuDobu={zmenitPrevadzkovuDobu} />
         </WindowModal>
       )}
 
