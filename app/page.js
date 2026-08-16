f6b46dbbfc9e5cf9e97b73250a45c869a187d2a0
EagleWatch77 <mirko@droppa.net>
2026-08-16 14:51:50 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -344,8 +344,7 @@ export default function PrehladPage() {
           <SezonaInfo prehlad={sezonnyPrehladInfo} jeMedzisezonaTeraz={jeTerazMedzisezona} />
         </div>
       )}
-
-      {turistiRozbaleni && (
+{otvorenyPanel === "turisti" && (
         <div style={{ position: "absolute", top: 66, left: 100, zIndex: 3 }}>
           <TuristiRozpis dennyPocetTuristov={dennyPocetTuristov} rozpisTuristovPodBudov={rozpisTuristovPodBudov} />
         </div>
