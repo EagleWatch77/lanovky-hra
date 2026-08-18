793289b93630412c74610f8130cc10c6fa498c42
EagleWatch77 <mirko@droppa.net>
2026-08-18 13:08:46 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -434,7 +434,7 @@ export default function PrehladPage() {
       <div style={{ position: "absolute", top: 70, right: 12, width: panelOtvoreny ? 250 : 110, zIndex: 3, display: "flex", flexDirection: "column", gap: 8 }}>
 
       {panelOtvoreny && <ZonaPanel stanica={stanica} budovy={budovy} efektivitaBudovy={efektivitaBudovy} onSpravovatZonu={() => setOkno("budovy")} />}
-        {panelOtvoreny && <RebricekPanel stanica={stanica} onOtvorRebricek={() => setOkno("rebricek")} />}
+
        
       </div>
     </div>
