82947c733c0c4636cc474c68c7e831f9353654cb
EagleWatch77 <mirko@droppa.net>
2026-08-18 16:13:14 +0200

    Update ZonaPanel.js

--- components/ZonaPanel.js
+++ components/ZonaPanel.js
@@ -1,8 +1,3 @@
-"use client";
-import { useState } from "react";
-import { ZONY, PORADIE_ZON, LANOVKY_TYPY, jeZimnyMesiac } from "../lib/katalog";
-import { hernyDatum } from "../lib/hernyCas";
-import { CableCar, Building2, Users, Star, Lock, Image as ImageIcon, ArrowRight } from "lucide-react";
 
 // Obrázky zón
 // Obrázky zón podľa sezóny. Ak letný chýba, použije sa zimný.
