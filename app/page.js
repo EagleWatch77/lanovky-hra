2f57dfc1d9542b18c96c9fa08471dafc16c79022
EagleWatch77 <mirko@droppa.net>
2026-08-18 14:09:21 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -436,7 +436,7 @@ export default function PrehladPage() {
 
       {/* Rebríček — zarovnaný na spodnú hranu */}
       <div style={{ position: "absolute", bottom: 12, right: 12, width: 250, zIndex: 3 }}>
-         <UdalostiPanel />
+       <UdalostiPanel onOtvorZamestnanci={() => setOkno("zamestnanci")} />
       </div>
         
     </div>
