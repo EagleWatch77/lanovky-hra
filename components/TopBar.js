dcad4c408a7de1d5a3ed1ebc96bddfd50802940d
EagleWatch77 <mirko@droppa.net>
2026-08-19 14:34:55 +0200

    Update TopBar.js

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,8 +1,7 @@
 "use client";
 import { hernyDatum } from "../lib/hernyCas";
 import { vypocitajDenoePocasie } from "../lib/pocasie";
-import { jeZimnyMesiac, jeMedzisezona } from "../lib/katalog";
-import { Star, Coins, Smile, Users, Calendar, Snowflake, Sun, Cloud } from "lucide-react";
+import { jeZimnyMesiac, jeMedzisezona } from "../lib/katalog";import { Star, Coins, Smile, Users, Calendar, Snowflake, Sun, CalendarOff } from "lucide-react";
 
 // ⚙️ PREPÍNAČ VZHĽADU IKON:
 //   "farebne"      = každá ikona má farbu svojej kategórie (zlatá minca, modrá hviezda...)
