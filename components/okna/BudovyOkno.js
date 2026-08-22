365b5cd8faa5d4ba19ca2d279f5532839b30b1b5
EagleWatch77 <mirko@droppa.net>
2026-08-22 13:08:40 +0200

    Add fallback for image source in BudovyOkno.js

--- components/okna/BudovyOkno.js
+++ components/okna/BudovyOkno.js
@@ -859,7 +859,7 @@ const jeParkovisko = kategoria === "parkovisko";
                         }}
                       >
                           <img
-                         src={OBRAZKY_LANOVIEK[typ] || OBRAZKY_PARKOVISK[typ] || OBRAZKY_UBYTOVANIA[typ] || OBRAZKY_SLUZIEB[typ]}
+                         src={OBRAZKY_LANOVIEK[typ] || OBRAZKY_PARKOVISK[typ] || OBRAZKY_UBYTOVANIA[typ] || OBRAZKY_SLUZIEB[typ] || OBRAZKY_POKLADNI[typ]}
                           alt=""
                           style={{ width: "100%", height: "100%", objectFit: "contain" }}
                         />
