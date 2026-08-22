ecbeda48b95e19df2a9c0e329ce247c0fbdaec7f
EagleWatch77 <mirko@droppa.net>
2026-08-22 09:52:29 +0200

    Update BudovyOkno.js

--- components/okna/BudovyOkno.js
+++ components/okna/BudovyOkno.js
@@ -839,7 +839,7 @@ const jeParkovisko = kategoria === "parkovisko";
                   }}
                 >
                <div style={{ display: "flex", alignItems: "center", gap: 11, minWidth: 0 }}>
-                   {(OBRAZKY_LANOVIEK[typ] || OBRAZKY_PARKOVISK[typ] || OBRAZKY_UBYTOVANIA[typ]) && (
+                 {(OBRAZKY_LANOVIEK[typ] || OBRAZKY_PARKOVISK[typ] || OBRAZKY_UBYTOVANIA[typ] || OBRAZKY_SLUZIEB[typ]) && (
                       <span
                         style={{
                           width: 64,
